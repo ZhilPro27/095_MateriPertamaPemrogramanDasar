37d23ead13c1b1a6e74f5b929bd3b5461fffe049
ZhilPro27 <zhilalkrisna@gmail.com>
2024-02-26 15:16:01 +0700

    Add project files.

---
+++ PertemuanPertamaPemrogramanDasar/PertemuanPertamaPemrogramanDasar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{// begin
+    // numeric nPanjang, nLebar, nLuas
+    int nPanjang, nLebar, nLuas;
+    // display 'Masukkan Panjang= '
+    cout << "Masukkan Panjang = ";
+    // accept nPanjang
+    cin >> nPanjang;
+    // display 'Masukkan Luas= '
+    cout << "Masukkan Lebar = ";
+    // accept nLuas
+    cin >> nLebar;
+    // compute nLuas = nPanjang * nLebar
+    nLuas = nPanjang * nLebar;
+    // display 'Luasnya adalah = ' + nLuas
+    cout << "Luasnya adalah = " << nLuas << endl;
+}// end
